b8e53799d19fe381fdf60b8b80aef681ecaaa9df
Ewerton Gonçalves <tonpublic97@gmail.com>
2023-09-18 23:10:26 -0300

    Aula 1 commit

--- src/pages/sizes/Sizes.tsx
+++ src/pages/sizes/Sizes.tsx
@@ -1,3 +1,77 @@
+import { useState } from "react";
+import { Button } from "../../components/button/Button";
+import { Layout } from "../../components/layout/Layout";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../routes";
+
 export default function Sizes() {
-  return <h1>Sizes</h1>
+
+  const navigate = useNavigate()
+  const sizeOptions = [
+  {
+    id: 10,
+    value: 'large',
+    text: 'Grande',
+  },
+  {
+    id: 11,
+    value: 'large-2',
+    text: 'Grande 2 Sabores',
+  },
+  {
+    id: 20,
+    value: 'medium',
+    text: 'Média',
+  },
+  {
+    id: 21,
+    value: 'medium-2',
+    text: 'Média 2 Sabores',
+  },
+  {
+    id: 30,
+    value: 'small',
+    text: 'Broto',
+  },
+  {
+    id: 31,
+    value: 'small-2',
+    text: 'Broto 2 Sabores',
+  },
+]
+
+const [size, setSize] = useState("") 
+const handleChange = (event) => {
+  setSize(event.target.value)
+}
+const handleBack = () => {
+  navigate(routes.home)
+}
+const handleNext = () => {
+  if(size === "") {
+    alert("Selecione o tamanho da pizza")
+    return
+  } else {
+    navigate(routes.pizzaFlavor)
+  }
+  
+}
+
+ return(
+  <Layout>
+    <h1>Escolha o tamanho da sua pizza</h1>
+    <section>
+      {sizeOptions.map(({id, value, text}) => ( 
+        <div key={id}>
+          <input type="radio" id={value} name="sizes" value={value} onChange={handleChange}/>
+          <label htmlFor={value}> {text}</label>
+        </div>
+      ))}
+    </section>
+    <div>
+      <Button onClick={handleBack}>Voltar</Button>
+      <Button onClick={handleNext}>Escolha o sabor</Button>
+    </div>
+  </Layout>
+ )
 }
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+export const routes = {
+    home: '/',
+    pizzaSize: '/pedido/escolha-o-tamanho',
+    pizzaFlavor: '/pedido/escolha-o-sabor',
+    summary: '/pedido/resumo',
+    checkout: '/checkout',
+    login: '/login',
+}
